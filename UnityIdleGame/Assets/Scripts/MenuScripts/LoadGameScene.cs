b5a0fb818314ffec1c48eb8a0a0284c084227ba5
Hevarius <Hevarius@users.noreply.github.com>
2018-06-11 05:09:18 -0400

    Add a Load Scene Functionality
    -Replaced Transition with LoadGameScene in the loadScene
    -Attempted to add loading bar, scene loads too quick to tell if working atm
    -added images for the loading bar

---
+++ UnityIdleGame/Assets/Scripts/MenuScripts/LoadGameScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameScene : MonoBehaviour {
+
+    public Texture2D emptyBar;
+    public Texture2D fullBar;
+    float time = 5.0f;
+
+    private AsyncOperation asyncLoad = null;
+
+	// Use this for initialization
+	void Start () {
+        time -= Time.deltaTime;
+        StartCoroutine(LoadScene());
+	}
+	
+	
+    //Load next scene in the background
+    IEnumerator LoadScene()
+    {
+        
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+       
+        while (!asyncLoad.isDone)
+            yield return null;
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 100, 50), emptyBar);
+        GUI.DrawTexture(new Rect(0, 0, 100 * asyncLoad.progress, 50), fullBar);
+    }
+}
